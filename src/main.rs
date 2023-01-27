157821b9c7ab12f510d40a3817be819126bd56e4
Noel Lopes <noel@ipg.pt>
2023-01-27 15:49:29 +0000

    Get the version from the metadata

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use std::io::Write;
 mod terminal_helper;
 
 fn main() {
-    const VERSION: &'static str = "0.0.1";
-
     let mut output = terminal_helper::Output::new();
 
     output.set_success_color();
     writeln!(&mut output.stdout, "XR Parser").ok();
 
     output.set_default_color();
-    writeln!(&mut output.stdout, "version {VERSION}").ok();
+    let version = env!("CARGO_PKG_VERSION");    
+    writeln!(&mut output.stdout, "version {version}").ok();
 }

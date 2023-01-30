a29c209506548a2cd409bd447aad2c8c57885edf
Noel Lopes <noel@ipg.pt>
2023-01-30 16:06:22 +0000

    Using clap to process command line options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use clap::Parser;
 use std::io::Write;
+use std::path::PathBuf;
 
 mod terminal_helper;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "XR parser", long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "./")]
+    directory: PathBuf,
+
+    #[arg(short, long)]
+    filenames: Vec<PathBuf>,
+}
 
 fn main() {
     let mut output = terminal_helper::Output::new();
@@ -9,6 +21,9 @@ fn main() {
     writeln!(&mut output.stdout, "XR Parser").ok();
 
     output.set_default_color();
-    let version = env!("CARGO_PKG_VERSION");    
+    let version = env!("CARGO_PKG_VERSION");
     writeln!(&mut output.stdout, "version {version}").ok();
+
+    let args = Args::parse();
+    println!("{:?}", args)
 }

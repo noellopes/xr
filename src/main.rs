56d3dfdc5edbc1680c1d5b4cb868015f8922a771
Noel Lopes <noel@ipg.pt>
2023-01-31 15:51:49 +0000

    Detect all xr files in the directory to process

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 use clap::Parser;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 
 mod terminal_helper;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "XR parser", long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "./")]
-    directory: PathBuf,
+    #[arg(short, long)]
+    directory: Option<PathBuf>,
 
     #[arg(short, long)]
     filenames: Vec<PathBuf>,
 }
 
+impl Args {
+    fn working_dir(self) -> PathBuf {
+        if let Some(dir) = self.directory {
+            dir
+        } else {
+            std::env::current_dir().unwrap_or(PathBuf::from("."))
+        }
+    }
+}
+
 fn main() {
     let mut output = terminal_helper::Output::new();
 
-    output.set_success_color();
-    writeln!(&mut output.stdout, "XR Parser").ok();
+    let args = Args::parse();
 
-    output.set_default_color();
+    writeln!(&mut output.stdout, "XR Parser").ok();
     let version = env!("CARGO_PKG_VERSION");
     writeln!(&mut output.stdout, "version {version}").ok();
 
-    let args = Args::parse();
-    println!("{:?}", args)
+    let mut files_processed = 0;
+
+    for entry in WalkDir::new(args.working_dir())
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let filename = entry.file_name();
+        if filename.to_string_lossy().to_lowercase().ends_with(".xr") {
+            process_file(entry.path());
+            files_processed += 1
+        }
+    }
+
+    output.set_success_color();
+    println!("{files_processed} files processed");
+    output.set_default_color();
+}
+
+fn process_file(path: &Path) {
+    println!("Processing file {:?}", path);
 }
